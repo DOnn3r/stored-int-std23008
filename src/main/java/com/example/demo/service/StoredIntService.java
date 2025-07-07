8dc9800b0efaf543b1624f8040d03ae2f38a41a3
Donner <152691192+DOnn3r@users.noreply.github.com>
2025-07-07 22:12:59 +0300

    feat: stored int

---
+++ src/main/java/com/example/demo/service/StoredIntService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import java.io.*;
+import java.util.Random;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoredIntService {
+
+    public int getStoredInt() throws IOException {
+        var filePath = "/tmp/stored-int.txt";
+        var file = new File(filePath);
+        if (!file.exists()) {
+            CreateAndWriteContent(file);
+        }
+
+        return readContent(file);
+    }
+
+    private int readContent(File file) throws IOException {
+        var reader = new BufferedReader(new FileReader(file));
+        return reader.read();
+    }
+
+    private void CreateAndWriteContent(File file) throws IOException {
+        var random = new Random();
+        var randomNumber = random.nextInt(100);
+
+        var writer = new FileWriter(file);
+        writer.write(randomNumber);
+        writer.close();
+    }
+}
